4b4a71782081f244cbde4673af3d5572a87145f3
Ethan Hocking <eshocking253@gmail.com>
2020-01-30 16:10:54 -0700

    Remove return statements, Rename @user_answer and user_answer to @guess and guess to match interaction pattern, Remove redundant accessor methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,29 +1,21 @@
 class Turn
-  attr_accessor :user_answer, :card
+  attr_reader :guess, :card
 
-  def initialize(user_answer, card)
-    @user_answer = user_answer
+  def initialize(guess, card)
+    @guess = guess
     @card = card
   end
 
-  def guess
-    return @user_answer
-  end
-
-  def card
-    return @card
-  end
-
   def correct?
-    return (@card.answer == user_answer)
+     @card.answer == guess
   end
 
   def feedback
-    if self.correct?
-      return "Correct!"
+    if correct?
+      "Correct!"
     else
-      return "Incorrect."
+      "Incorrect."
     end
   end
-  
+
 end
