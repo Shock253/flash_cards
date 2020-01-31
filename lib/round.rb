aa5e1ad8bb0b77868700007a04f9e7b796c6226c
Ethan Hocking <eshocking253@gmail.com>
2020-01-30 18:57:59 -0700

    Add take_turn method to Round class and associated testing

--- lib/round.rb
+++ lib/round.rb
@@ -2,14 +2,25 @@ require "./lib/deck"
 
 
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
   def initialize(deck)
     @deck = deck
     @turns = []
     @current_card_index = 0
+    @number_correct = 0
   end
 
   def current_card
     @deck.cards[@current_card_index]
+  end
+
+  def take_turn(user_answer)
+    new_turn = Turn.new(user_answer, @deck.cards[@current_card_index])
+    @turns << new_turn
+    if new_turn.correct?
+      @number_correct += 1
+    end
+
+    new_turn
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -39,14 +39,34 @@ class RoundTest < Minitest::Test
 
     assert_equal card1, round.current_card
   end
+
+  def test_can_take_turn
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card1, card2, card3])
+    round = Round.new(deck)
+
+
+    new_turn = round.take_turn("Juneau")
+
+    assert_instance_of Turn, new_turn
+    assert_equal true, new_turn.correct?
+    assert_equal [new_turn], round.turns
+    assert_equal 1, round.number_correct
+    assert_instance_of Card, round.current_card
+  end
 end
 
 # pry(main)> new_turn.correct?
 # #=> true
 #
+
 # pry(main)> round.turns
 # #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
 #
+
 # pry(main)> round.number_correct
 # #=> 1
 #
