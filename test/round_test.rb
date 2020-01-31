b888ed0d93d2fa3a8b39b836e38b24d57aa4415b
Ethan Hocking <eshocking253@gmail.com>
2020-01-30 18:39:11 -0700

    Renamed card variables and method names for readability

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,52 +7,30 @@ require './lib/round'
 
 class RoundTest < Minitest::Test
   def test_it_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    deck = Deck.new([card_1, card_2, card_3])
+    deck = Deck.new([card1, card2, card3])
     round = Round.new(deck)
 
     assert_instance_of Round, round
   end
 
-  def test_can_access_deck_attribute
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  def test_has_attributes
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    deck = Deck.new([card_1, card_2, card_3])
+    deck = Deck.new([card1, card2, card3])
     round = Round.new(deck)
 
     assert_equal deck, round.deck
-  end
-
-  def test_has_empty_turns_attribute
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
-
     assert_equal [], round.turns
   end
 
 end
 
-# pry(main)> round.turns
-# #=> []
-#
-# pry(main)> round.current_card
-# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
-#
-# pry(main)> new_turn = round.take_turn("Juneau")
-# #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
-#
-# pry(main)> new_turn.class
-# #=> Turn
-#
 # pry(main)> new_turn.correct?
 # #=> true
 #
@@ -65,6 +43,7 @@ end
 # pry(main)> round.current_card
 # #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
 #
+
 # pry(main)> round.take_turn("Venus")
 # #=> #<Turn:0x00007f972a215b38...>
 #
