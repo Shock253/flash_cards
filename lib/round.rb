70cfc918d1540eba7a9023795e11b7b492f97959
Ethan Hocking <eshocking253@gmail.com>
2020-02-01 11:52:31 -0700

    Add percent_correct_by category method to Round class and associated tests

--- lib/round.rb
+++ lib/round.rb
@@ -39,4 +39,14 @@ class Round
     (@number_correct.to_f / turns.length.to_f) * 100.0
   end
 
+  def percent_correct_by_category(category)
+    number_in_category = 0
+    @turns.each do |turn|
+      if turn.card.category == category
+        number_in_category += 1
+      end
+    end
+
+    (number_correct_by_category(category).to_f / number_in_category.to_f) * 100
+  end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -100,6 +100,22 @@ class RoundTest < Minitest::Test
     assert_equal 50.0, round.percent_correct
 
   end
+
+  def test_calculates_percent_correct_by_category
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card1, card2, card3])
+    round = Round.new(deck)
+
+
+    new_turn1 = round.take_turn("Juneau")
+    new_turn2 = round.take_turn("Venus")
+
+    assert_equal 100.0, round.percent_correct_by_category(:Geography)
+    assert_equal 0.0, round.percent_correct_by_category(:STEM)
+  end
 end
 
 # pry(main)> round.percent_correct
